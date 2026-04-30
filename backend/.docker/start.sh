ac3eb5acf70dbe2e0adbc5ba9bffd0bb6a8f9811
rizts <rizts.tech@gmail.com>
2026-04-30 17:52:49 +0700

    feat: dockerize backend and frontend for Render.com deployment

---
+++ backend/.docker/start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Set default value for RUN_WORKER if not set
+if [ -z "$RUN_WORKER" ]; then
+    export RUN_WORKER=false
+fi
+
+# Optimization for Laravel
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Start supervisor
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
